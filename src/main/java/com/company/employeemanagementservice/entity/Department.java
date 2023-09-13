5fb7972bbaa9cb56e2efcc86bda2ba63add7a325
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-13 22:34:28 +0300

    Entity package is added.

---
+++ src/main/java/com/company/employeemanagementservice/entity/Department.java
@@ -0,0 +1,27 @@
+package com.company.employeemanagementservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long budget;
+    private int employeeCount;
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+}
---
+++ src/main/java/com/company/employeemanagementservice/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.company.employeemanagementservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+
+    private String email;
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+    private String bankAccountNumber;
+
+
+}
