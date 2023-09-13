b29685719f5973ee08c4f0897c9adb1a199320ac
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-13 22:36:52 +0300

    Service interface classes of Service package is added.

---
+++ src/main/java/com/company/employeemanagementservice/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.company.employeemanagementservice.service;
+
+import com.company.employeemanagementservice.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    boolean updateEmployee(Long employeeId, EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getEmployeeList();
+
+    boolean deleteEmployee(Long employeeId);
+}
